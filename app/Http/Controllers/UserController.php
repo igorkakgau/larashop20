ceaca8dba8681889f53545c94278b0cc3d199e33
igorkakgau <mindalev@rambler.ru>
2020-03-18 20:01:46 +0300

    'Added'

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+use App\Http\Resources\UserResource as UserResource;
+class UserController extends Controller
+{
+    public function getusers()
+    {
+        $users = User::get();
+        return UserResource::collection($users);
+    }
+}
